51c79561f3a0ecb95a92390a583acd07ad6f013b
buzz1710 <neil.armstrong@sage.com>
2016-02-08 16:25:03 +0000

    address comments

--- src/org/sage_one_sample/sageone/SageoneToken.java
+++ src/org/sage_one_sample/sageone/SageoneToken.java
@@ -18,13 +18,6 @@ public class SageoneToken extends HttpServlet {
   private static final long serialVersionUID = 1L;
 
   /**
-  * @see HttpServlet#HttpServlet()
-  */
-  public SageoneToken() {
-    super();
-    // TODO Auto-generated constructor stub
-  }
-  /**
   * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
   */
   protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
@@ -84,7 +77,6 @@ public class SageoneToken extends HttpServlet {
   * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
   */
   protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-    // TODO Auto-generated method stub
     doGet(request, response);
   }
 }
